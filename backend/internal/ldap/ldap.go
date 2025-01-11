a4f281e8792e5119293d70e1bacf82b4f313fde7
Kyle Mendell <kmendell@ofkm.us>
2025-01-11 12:03:33 -0600

    feat(ldap-sync): added basic ldapInit() function

--- backend/internal/ldap/ldap.go
+++ backend/internal/ldap/ldap.go
@@ -1,1 +1,27 @@
 package ldap
+
+import (
+	"crypto/tls"
+
+	"github.com/go-ldap/ldap/v3"
+	"github.com/stonith404/pocket-id/backend/internal/common"
+)
+
+ func ldapInit() *ldap.Conn {
+	// Setup AD Connection
+	ldapURL := common.EnvConfig.LDAPServer + common.EnvConfig.LDAPPort
+	client, err := ldap.DialURL(ldapURL, ldap.DialWithTLSConfig(&tls.Config{InsecureSkipVerify: common.EnvConfig.LDAPTLSVerify}))
+	if err != nil {
+		//TODO Handle Errors Better
+		panic(err)
+	}
+	// defer client.Close()
+
+	// Bind as Service Account
+	err = client.Bind(common.EnvConfig.LDAPBindUser, common.EnvConfig.LDAPBindPassword)
+	if err != nil {
+		//TODO Handle Errors Better
+		panic(err)
+	}
+	return client
+ }
