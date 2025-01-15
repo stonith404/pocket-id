704b2a88fc5958ab4cda9a89b42780668f5b6292
Kyle Mendell <kmendell@ofkm.us>
2025-01-15 16:51:11 -0600

    sync ldap users into ldap groups (WIP)

--- backend/internal/service/ldap_service.go
+++ backend/internal/service/ldap_service.go
@@ -4,6 +4,7 @@ import (
 	"crypto/tls"
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/go-ldap/ldap/v3"
 	"github.com/stonith404/pocket-id/backend/internal/common"
@@ -57,9 +58,26 @@ func (s *LdapService) GetLdapGroups() error {
 
 	for _, value := range result.Entries {
 
+		var usersToAddDto dto.UserGroupUpdateUsersDto
+		var userIDStrings []string
+		var groupMemberAddError error
+
+		// Check if Group Already exsists
 		var databaseGroup model.UserGroup
 		groupUniqueName := value.GetAttributeValue(common.EnvConfig.LDAPGroupAttribute)
 		s.db.Where("name = ?", groupUniqueName).First(&databaseGroup)
+
+		//Get Group Members and add to the correct Group
+		groupMembers := value.GetAttributeValues("member")
+		for _, member := range groupMembers {
+			//Normal output of this would be CN=username,ou=people,dc=example,dc=com
+			// Splitting at the = and , then just grabbing the username for that string
+			singleMember := strings.Split(strings.Split(member, "=")[1], ",")[0]
+
+			var databaseUser model.User
+			s.db.Where("username = ?", singleMember).First(&databaseUser)
+			userIDStrings = append(userIDStrings, databaseUser.ID)
+		}
 
 		syncGroup := dto.UserGroupCreateDto{
 			Name:         value.GetAttributeValue(common.EnvConfig.LDAPGroupAttribute),
@@ -67,14 +85,19 @@ func (s *LdapService) GetLdapGroups() error {
 		}
 
 		if databaseGroup.ID == "" {
-			_, err = s.groupService.Create(syncGroup)
-			if err != nil {
-				log.Printf("Error syncing group %s: %s", syncGroup.Name, err)
+			var newGroup model.UserGroup
+			newGroup, groupMemberAddError = s.groupService.Create(syncGroup)
+			_, groupMemberAddError = s.groupService.UpdateUsers(newGroup.ID, usersToAddDto)
+			if groupMemberAddError != nil {
+				log.Printf("Error syncing group %s: %s", syncGroup.Name, groupMemberAddError)
+				return groupMemberAddError
 			}
 		} else {
-			_, err := s.groupService.Update(databaseGroup.ID, syncGroup)
-			if err != nil {
-				log.Printf("Error syncing group %s: %s", syncGroup.Name, err)
+			_, groupMemberAddError = s.groupService.Update(databaseGroup.ID, syncGroup)
+			_, groupMemberAddError = s.groupService.UpdateUsers(databaseGroup.ID, usersToAddDto)
+			if groupMemberAddError != nil {
+				log.Printf("Error syncing group %s: %s", syncGroup.Name, groupMemberAddError)
+				return groupMemberAddError
 			}
 
 		}
