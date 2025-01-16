2116ef7be42fd86d4e0899f65cc6e258f05fdac6
Elias Schneider <login@eliasschneider.com>
2025-01-16 14:59:57 +0100

    undo type assertion changes in `package.json`

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,6 +1,6 @@
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import packageJson from './package.json' with { type: 'json' };
+import packageJson from './package.json' assert { type: 'json' };
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
