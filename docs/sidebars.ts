c12bf2955b99fb6ae2274234bf6e923013d39b17
Kyle Mendell <kmendell@ofkm.us>
2025-01-21 11:46:42 -0600

    docs: add docusaurus docs (#118)
    Co-authored-by: Elias Schneider <login@eliasschneider.com>

---
+++ docs/sidebars.ts
@@ -0,0 +1,98 @@
+import type { SidebarsConfig } from "@docusaurus/plugin-content-docs";
+
+const sidebars: SidebarsConfig = {
+  docsSidebar: [
+    {
+      type: "doc",
+      id: "introduction",
+    },
+    {
+      type: "category",
+      label: "Getting Started",
+      items: [
+        {
+          type: "doc",
+          id: "setup/installation",
+        },
+        {
+          type: "doc",
+          id: "setup/nginx-reverse-proxy",
+        },
+        {
+          type: "doc",
+          id: "setup/upgrading",
+        },
+      ],
+    },
+    {
+      type: "category",
+      label: "Configuration",
+      items: [
+        {
+          type: "doc",
+          id: "configuration/environment-variables",
+        },
+        {
+          type: "doc",
+          id: "configuration/ldap",
+        },
+      ],
+    },
+    {
+      type: "category",
+      label: "Guides",
+      items: [
+        {
+          type: "doc",
+          id: "guides/proxy-services",
+        },
+      ],
+    },
+    {
+      type: "category",
+      label: "Client Examples",
+      link: {
+        type: "generated-index",
+        title: "Client Examples",
+        description:
+          "Examples of how to setup Pocket ID with different clients",
+        slug: "client-examples",
+      },
+      items: [
+        "client-examples/hoarder",
+        "client-examples/jellyfin",
+        "client-examples/vikunja",
+        "client-examples/open-webui",
+        "client-examples/semaphore-ui",
+        "client-examples/cloudflare-zero-trust",
+      ],
+    },
+    {
+      type: "category",
+      label: "Troubleshooting",
+      items: [
+        {
+          type: "doc",
+          id: "troubleshooting/account-recovery",
+        },
+      ],
+    },
+    {
+      type: "category",
+      label: "Helping Out",
+      items: [
+        {
+          type: "doc",
+          id: "help-out/contribute",
+        },
+      ],
+    },
+    {
+      type: "link",
+      label: "Demo",
+      href: "https://pocket-id.eliasschneider.com/",
+    },
+  ],
+};
+
+export default sidebars;
