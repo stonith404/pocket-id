32d993de76c6c47039c0e2efea86d2b20725d76b
Elias Schneider <login@eliasschneider.com>
2025-01-28 20:16:59 +0100

    show version label on mobile

--- docs/src/version-label.ts
+++ docs/src/version-label.ts
@@ -1,20 +1,23 @@
-import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
+import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
 
 if (ExecutionEnvironment.canUseDOM) {
+  function readVersionFile() {
+    return fetch(
+      "https://raw.githubusercontent.com/stonith404/pocket-id/refs/heads/main/.version"
+    )
+      .then((response) => response.text())
+      .catch((error) => `Error reading version file: ${error}`);
+  }
 
-    function readVersionFile() {
-        return fetch('https://raw.githubusercontent.com/stonith404/pocket-id/refs/heads/main/.version')
-          .then(response => response.text())
-          .catch(error => `Error reading version file: ${error}`);
-      }
-
-    function getVersion() {
-        readVersionFile().then(version => {
-          const navbarItem = document.querySelector('.navbar__item[href="#version"]');
-          if (navbarItem) {
-            navbarItem.innerHTML = version;
-          }
-        }).catch(error => console.error('Error fetching version:', error));
-      }
-    window.addEventListener("load", getVersion);
+  function getVersion() {
+    readVersionFile()
+      .then((version) => {
+        const versionLabels = document.querySelectorAll('[href="#version"]');
+        versionLabels.forEach((label) => {
+          (label as HTMLElement).innerText = `v${version}`;
+        });
+      })
+      .catch((error) => console.error("Error fetching version:", error));
+  }
+  window.addEventListener("load", getVersion);
 }
