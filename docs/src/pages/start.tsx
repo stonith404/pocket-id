3314ddf362a57f69f32e99a558508a0452b606f6
Kyle Mendell <kmendell@ofkm.us>
2025-02-04 14:52:20 -0600

    fix header text to be even with image

--- docs/src/pages/start.tsx
+++ docs/src/pages/start.tsx
@@ -5,17 +5,17 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 const Home: React.FC = () => {
   return (
     <div className="text-white h-screen flex flex-col bg-muted/40" >
-      <header className="w-full border-b" style={{ backgroundColor: "hsl(240, 10%, 3.9%)" }}>
-        <div className="max-w-[1640px] mx-auto flex w-full items-center justify-between px-4 md:px-10">
-          {/* <div className="flex items-center w-full justify-between"> */}
-            <div className="flex h-16 items-center">
+      <header className="" style={{ backgroundColor: "hsl(240, 10%, 3.9%)" }}>
+        <div className="w-full border-b">
+          <div className="max-w-[1640px] mx-auto flex w-full items-center justify-between px-4 md:px-10">
+            <div className="flex h-16 items-center justify">
               <img src="https://docs.pocket-id.org/img/pocket-id.png" alt="Pocket ID Logo" className="mr-3 h-8 w-8" />
-              <h1 className="text-md font-medium">Pocket ID</h1>
+              <h1 className="text-lg font-medium" style={{ margin: 0 }} >Pocket ID</h1>
             </div>
             <a href="https://github.com/stonith404/pocket-id" target="_blank" rel="noopener noreferrer" style={{ color: "hsl(0, 0%, 98%)" }} className="text-white text-2xl">
               <i className="fab fa-github" aria-hidden="true"></i>
             </a>
-          {/* </div> */}
+          </div>
         </div>
       </header>
 
