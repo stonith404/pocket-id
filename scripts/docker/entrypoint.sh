e864d5dcbff1ef28dc6bf120e4503093a308c5c8
Elias Schneider <login@eliasschneider.com>
2025-02-05 18:14:49 +0100

    feat: add option to disable Caddy in the Docker container

--- scripts/docker/entrypoint.sh
+++ scripts/docker/entrypoint.sh
@@ -4,13 +4,17 @@ node frontend/build &
 echo "Starting backend..."
 cd backend && ./pocket-id-backend &
 
-echo "Starting Caddy..."
+if [ "$CADDY_DISABLED" != "true" ]; then
+  echo "Starting Caddy..."
 
-# Check if TRUST_PROXY is set to true and use the appropriate Caddyfile
-if [ "$TRUST_PROXY" = "true" ]; then
-  caddy start --adapter caddyfile --config /etc/caddy/Caddyfile.trust-proxy &
+  # Check if TRUST_PROXY is set to true and use the appropriate Caddyfile
+  if [ "$TRUST_PROXY" = "true" ]; then
+    caddy start --adapter caddyfile --config /etc/caddy/Caddyfile.trust-proxy &
+  else
+    caddy start --adapter caddyfile --config /etc/caddy/Caddyfile &
+  fi
 else
-  caddy start --adapter caddyfile --config /etc/caddy/Caddyfile &
+  echo "Caddy is disabled. Skipping..."
 fi
 
 wait
