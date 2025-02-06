3a6fce5c4bcfbe4d9efdf586df31da82a149d11c
Kyle Mendell <kmendell@ofkm.us>
2025-02-06 09:41:44 -0600

    docs: add landing page  (#203)
    Co-authored-by: Elias Schneider <login@eliasschneider.com>

---
+++ docs/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
